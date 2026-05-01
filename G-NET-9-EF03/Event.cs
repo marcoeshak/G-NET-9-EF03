1fcc25dafdf0765e407597299bbcbfc85862dff3
marco <marco@DESKTOP-GCUIOA1>
2026-05-01 22:26:42 +0300

    class Event

---
+++ G-NET-9-EF03/Event.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_NET_9_EF03
+{
+    public class Event
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+
+        public int MaxAttendees { get; set; }
+
+        public int OrganizerId { get; set; }
+
+        public Organizer Organizer { get; set; }
+
+        public int? ParentEventId { get; set; }
+
+        public Event ParentEvent { get; set; }
+
+        public ICollection<Event> Sessions { get; set; } = new HashSet<Event>();
+
+        public ICollection<Registration> Registrations { get; set; } = new HashSet<Registration>();
+    }
+}
