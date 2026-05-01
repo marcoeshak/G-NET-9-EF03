7eb965e9ce1ff6a28675a4bb5190304094f1eda8
marco <marco@DESKTOP-GCUIOA1>
2026-05-01 22:28:59 +0300

    class Badge

---
+++ G-NET-9-EF03/Badge.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_NET_9_EF03
+{
+    public class Badge
+    {
+        public int Id { get; set; }
+
+        public string BadgeNumber { get; set; }
+
+        public DateTime IssuedDate { get; set; }
+
+        public string Tier { get; set; }
+
+        public int AttendeeId { get; set; }
+
+        public Attendee Attendee { get; set; }
+    }
+}
