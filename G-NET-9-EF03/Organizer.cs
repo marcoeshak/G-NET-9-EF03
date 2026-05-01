bc347c497180f8f85df397937926d8894c27ad1f
marco <marco@DESKTOP-GCUIOA1>
2026-05-01 22:23:15 +0300

    class Organizer

---
+++ G-NET-9-EF03/Organizer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_NET_9_EF03
+{
+    public class Organizer
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string? CompanyName { get; set; }
+
+        public bool IsVerified { get; set; }
+
+        public OrganizerProfile Profile { get; set; }
+
+        public ICollection<Event> Events { get; set; } = new HashSet<Event>();
+    }
+}
