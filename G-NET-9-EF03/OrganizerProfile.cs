c7b1c73c230025fed98d561575db69a94ffb0ace
marco <marco@DESKTOP-GCUIOA1>
2026-05-01 22:25:36 +0300

    class OrganizerProfile

---
+++ G-NET-9-EF03/OrganizerProfile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_NET_9_EF03
+{
+    public class OrganizerProfile
+    {
+        public int Id { get; set; }
+
+        public string Bio { get; set; }
+
+        public string WebsiteUrl { get; set; }
+
+        public string LogoUrl { get; set; }
+
+        public int OrganizerId { get; set; }
+
+        public Organizer Organizer { get; set; }
+    }
+}
